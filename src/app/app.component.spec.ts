b88922445a0ee356fa9a82af945ff3623ae8189c
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-07-05 21:49:56 +0530

    correct project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dtn215'`, () => {
+  it(`should have as title 'dnt215'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dtn215');
+    expect(app.title).toEqual('dnt215');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dtn215 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('dnt215 app is running!');
   });
 });
