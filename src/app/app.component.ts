31c2eb99445d4ce55ce0be6b0e74097849f91e46
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-07-05 22:58:36 +0530

    inital checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello World</h1>
+  //   <h2>From Angular</h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
